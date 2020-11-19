ce2be4782ce2c1b947d2ae6455cd5bdf8fcad4b6
Denis Peshekhonov <denis@peshekhonov.ru>
2020-11-19 22:55:44 +0300

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png">
+        <HelloWorld msg="Welcome to Your Vue.js App"/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
+    name: 'App',
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+    -webkit-user-select: none;
+    user-select: none;
+    overscroll-behavior-y: none;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+a, a:active, a:focus{
+    outline: none!important;
+}
+html,
+body {
+    position: fixed;
+    overflow: hidden;
 }
 </style>
